ac4fa700a24fdcdea17b91b19c03017e830a9319
ETINAweb237 <petitdassi7@gmail.com>
2023-09-20 13:49:12 +0100

    modification de la page d'acceuil

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
 
-        <div class="relative mt-4 py-4">
+        <div class="relative mt-4 py-4 flex">
            <h1 class="text-info text-uppercase text-center">Initiation Laravel</h1>
            <h2 class="text-center">La formation en programmation web et mobile chez 3IA_SIR_TECH</h2>
-           <p class=" justify-content-center text-align-center align-items-center"><img src="images\Image3ia2.png" alt="logo 3ia" style="width:70px;height:85px;"></p>
+           <p class=" justify-content-center align-items-center"><img src="{{asset('/images\Image3ia2.png')}}" alt="logo 3ia" style="width:200px;heigth:185px;"></p>
            <h4 class="text-center text-lowercase"> il est a present <?= date('h:i:s A')?>.</h4>
            <h4  class="text-center text-capitalize"> il est a present {{date('h:i:s A')}}....et il fait du beau temps ici au pays</h4>
         </div>
